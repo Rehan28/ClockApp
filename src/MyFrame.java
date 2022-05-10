39b26a5585e2bbe722e46c5a8b09e8cf9b01b1db
Rehan28 <71427882+Rehan28@users.noreply.github.com>
2022-05-10 17:02:59 +0600

    Done

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -7,22 +7,26 @@ public class MyFrame extends JFrame {
 
     Calendar calendar;
     SimpleDateFormat timeFormate;
-    SimpleDateFormat dayFormat;
+    SimpleDateFormat dayFormate;
+    SimpleDateFormat dateFormate;
     JLabel timeLabele;
+    JLabel dateLabele;
     JLabel dayLabele;
     String day;
     String time;
+    String date;
 
     MyFrame()
     {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("My Clock");
         this.setLayout(new FlowLayout());
-        this.setSize(350,200);
+        this.setSize(350,230);
         this.setResizable(false);
 
         timeFormate = new SimpleDateFormat("hh:mm:ss a");
-        dayFormate = new SimpleDateFormat("E");
+        dayFormate = new SimpleDateFormat("EEEE");
+        dateFormate = new SimpleDateFormat(" dd - MMMMM - yyyy");
 
         timeLabele = new JLabel();
         timeLabele.setFont(new Font("Vardana",Font.PLAIN,50));
@@ -31,11 +35,14 @@ public class MyFrame extends JFrame {
         timeLabele.setOpaque(true);
 
         dayLabele = new JLabel();
-        dayLabele.setFont(new Font("Ink Free",Font.PLAIN,50));
+        dayLabele.setFont(new Font("Roboto Mono",Font.PLAIN,40));
 
+        dateLabele = new JLabel();
+        dateLabele.setFont(new Font("Roboto Mono",Font.PLAIN,30));
 
         this.add(timeLabele);
         this.add(dayLabele);
+        this.add(dateLabele);
         this.setVisible(true);
         setTime();
     }
@@ -44,6 +51,12 @@ public class MyFrame extends JFrame {
         while (true){
             time = timeFormate.format(Calendar.getInstance().getTime());
             timeLabele.setText(time);
+
+            day = dayFormate.format(Calendar.getInstance().getTime());
+            dayLabele.setText(day);
+
+            date = dateFormate.format(Calendar.getInstance().getTime());
+            dateLabele.setText(date);
 
             try {
                 Thread.sleep(1000);
